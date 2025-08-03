0ac0755985165d1b89dbbd0db5ca9d22dcd62ad9
tekkengreg <greg@tekkengreg.com>
2025-08-03 11:26:23 +0200

    feat: add compile task and update webview settings

--- main.ts
+++ main.ts
@@ -1,20 +1,28 @@
-import { Webview } from "@webview/webview";
+import { SizeHint, Webview } from "@webview/webview";
 
-const html = `
-  <html>
-  <body>
-    <h1>Hello from deno v${Deno.version.deno}</h1>
-  </body>
-  </html>
-`;
+// const html = `
+//   <html>
+//   <body>
+//     <h1>Hello from deno v${Deno.version.deno}</h1>
+//   </body>
+//   </html>
+// `;
 
-const buttonWV = new Webview();
+// const buttonWV = new Webview();
 
-buttonWV.navigate(`data:text/html,${encodeURIComponent(html)}`);
+// buttonWV.navigate(`data:text/html,${encodeURIComponent(html)}`);
 
 
 const webview = new Webview();
 webview.navigate(Deno.args[0] || "https://www.wikipedia.org");
 
+webview.size = { width: 512, height: 1024, hint: SizeHint.NONE };
+webview.title = "BigBrowser";
 
-await Promise.all([buttonWV.run(), webview.run()]);
+
+
+
+// await Promise.all([buttonWV.run(), webview.run()]);
+await webview.run();
+
+
